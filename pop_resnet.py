9debdd6c3e3320ad9c407957079df117c2d6c95f
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-10-19 19:32:14 +0200

    config on MUS training data

--- pop_resnet.py
+++ pop_resnet.py
@@ -11,8 +11,8 @@ _WIDTH = 15
 _NUM_CHANNELS = 3
 _NUM_CLASSES = 88
 _NUM_IMAGES = {
-    'train': 76634,
-    'validation': 2000,
+    'train': 208374,
+    'validation': 38678,
 }
 
 
@@ -95,7 +95,7 @@ def resnet_model_fn(features, labels, mode, params):
 
     learning_rate_fn = pop_resnet_model_fn.learning_rate_with_decay(
         batch_size=params['batch_size'], batch_denom=params['batch_size'],
-        num_images=_NUM_IMAGES['train'], boundary_epochs=[2, 4, 6],  # boundary_epochs=[100, 150, 200],
+        num_images=_NUM_IMAGES['train'], boundary_epochs=[10, 20, 30],  # boundary_epochs=[100, 150, 200],
         decay_rates=[1, 0.1, 0.01, 0.001])
 
     # We use a weight decay of 0.0002, which performs better
--- pop_resnet_main.py
+++ pop_resnet_main.py
@@ -39,7 +39,7 @@ num_examples = 208374
 num_val_examples = 38678
 batch_size = 128
 steps_per_epoch = int(round(num_examples/batch_size))
-train_epochs = 10
+train_epochs = 40
 total_train_steps = train_epochs * steps_per_epoch
 
 run_params = {
@@ -94,7 +94,7 @@ def main(argv):
         eval_spec = tf.estimator.EvalSpec(input_fn=lambda: dataset.tfrecord_val_input_fn(val_dataset_tfrecord,
                                                                                          batch_size=run_params['batch_size'],
                                                                                          num_epochs=1),
-                                          steps=run_params['eval_steps'], start_delay_secs=3600, throttle_secs=1800)
+                                          steps=run_params['eval_steps'], start_delay_secs=3600, throttle_secs=3600)
 
         tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
 
