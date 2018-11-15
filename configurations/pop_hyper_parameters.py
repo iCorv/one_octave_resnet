b1a9b7d49bb5084a0e7f5ffaa988cf24179ae3bf
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-11-15 10:46:26 +0100

    minor hparam adjustments

---
+++ configurations/pop_hyper_parameters.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+
+DEFAULT_DTYPE = tf.float32
+
+num_examples = 482952 #1042876 #4163882
+num_val_examples = 87628 #792567
+batch_size = 8
+batches_per_epoch = int(round(num_examples/batch_size))
+train_epochs = 20
+total_train_steps = train_epochs * batches_per_epoch
+
+
+def get_hyper_parameters(net):
+    if net == 'ConvNet':
+        config = {'batch_size': batch_size,
+                  'dtype': DEFAULT_DTYPE,
+                  'clip_norm': 1e-7,
+                  # initial learning rate
+                  'learning_rate': 0.1,
+                  # when to change learning rate
+                  'boundary_epochs': [0.7, 2, 3],
+                  # factor by which the initial learning rate is multiplied (needs to be one more than the boundaries)
+                  'decay_rates': [1., 1., 1., 1.],
+                  'momentum': 0.9,
+                  'frames': 5,
+                  'freq_bins': 229,
+                  'num_channels': 1,
+                  'num_classes': 88,
+                  'num_examples': num_examples,
+                  'num_val_examples': num_val_examples,
+                  'batches_per_epoch': batches_per_epoch,
+                  'train_steps': total_train_steps,
+                  'eval_steps': int(round(num_val_examples/batch_size)),
+                  'data_format': 'channels_last',
+                  'train_epochs': train_epochs}
+    elif net == 'ResNet':
+        config = {}
+    else:
+        config = {}
+    return config
