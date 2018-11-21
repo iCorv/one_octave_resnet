75b2deb759982ea637b059b586460ecf26602cd3
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-11-21 11:07:47 +0100

    fold_2 config

--- pop_input_data.py
+++ pop_input_data.py
@@ -317,11 +317,11 @@ def tfrecord_train_input_fn(filepath, batch_size, num_epochs):
     # Map the parser over dataset, and batch results by up to batch_size
     # dataset = dataset.shuffle(100000)
     # dataset = dataset.repeat(num_epochs)
-    dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(100000, num_epochs))
+    dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(10000, num_epochs))
     dataset = dataset.apply(tf.contrib.data.map_and_batch(tfrecord_train_parser, batch_size))
     # dataset = dataset.map(tfrecord_train_parser)
     # dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(1)
+    dataset = dataset.prefetch(batch_size)
 
     return dataset
 
@@ -334,9 +334,9 @@ def tfrecord_val_input_fn(filepath, batch_size, num_epochs):
     # dataset = dataset.repeat(num_epochs)
     # dataset = dataset.map(tfrecord_train_parser)
     # dataset = dataset.batch(batch_size)
-    dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(100000, num_epochs))
+    dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(10000, num_epochs))
     dataset = dataset.apply(tf.contrib.data.map_and_batch(tfrecord_train_parser, batch_size))
-    dataset = dataset.prefetch(1)
+    dataset = dataset.prefetch(batch_size)
 
     return dataset
 
