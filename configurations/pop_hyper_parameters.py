314e15cb1a3b13890ec1ebcff2d95d6fe4cd18a4
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-11-21 08:40:11 +0100

    convnet config

--- configurations/pop_hyper_parameters.py
+++ configurations/pop_hyper_parameters.py
@@ -8,7 +8,7 @@ num_val_examples = 749017  # 749017  # 87628
 num_test_examples = 1570005
 batch_size = 128
 batches_per_epoch = int(round(num_examples/batch_size))
-train_epochs = 50
+train_epochs = 60
 total_train_steps = train_epochs * batches_per_epoch
 
 
@@ -32,9 +32,9 @@ def get_hyper_parameters(net):
                   # initial learning rate
                   'learning_rate': 1.0,
                   # when to change learning rate
-                  'boundary_epochs': [10, 20, 30], #[epoch for epoch in frange(0, train_epochs, train_epochs/60)][0:60],
+                  'boundary_epochs': [10, 20, 30, 40], #[epoch for epoch in frange(0, train_epochs, train_epochs/60)][0:60],
                   # factor by which the initial learning rate is multiplied (needs to be one more than the boundaries)
-                  'learning_rate_cycle': [0.1, 0.5*0.1, 0.5*0.5*0.1, 0.5*0.5*0.5*0.1], #[learning_rate for learning_rate in frange(10e-5, 1., (1. - 10e-5) / (30. + 2.))][
+                  'learning_rate_cycle': [0.1, 0.5*0.1, 0.5*0.5*0.1, 0.5*0.5*0.5*0.1, 0.5*0.5*0.5*0.5*0.1], #[learning_rate for learning_rate in frange(10e-5, 1., (1. - 10e-5) / (30. + 2.))][
                                          #0:30] + [learning_rate for learning_rate in frange(1., 10e-5, (1. - 10e-5) / (30. + 2.))][
                                          #0:31],
                   'decay_rates': [10e-5, 10e-4, 10e-3, 10e-2, 10e-1, 1, 10e-1, 10e-2, 10e-3, 10e-4, 10e-3],
