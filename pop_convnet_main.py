24bf33554b1bab43620ada6b95a25ca99db51c82
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-11-23 10:07:19 +0100

    test fold_1

--- pop_convnet_main.py
+++ pop_convnet_main.py
@@ -24,10 +24,10 @@ DEFAULT_DTYPE = tf.float32
 
 TEST_ID = 1
 
-train_and_val = True
+train_and_val = False
 predict_flag = False
 train_flag = False
-eval_flag = False
+eval_flag = True
 
 hparams = php.get_hyper_parameters('ConvNet')
 
@@ -82,7 +82,7 @@ def main(_):
             input_fn=lambda: dataset.tfrecord_val_input_fn(test_dataset_tfrecord,
                                                            batch_size=hparams['batch_size'],
                                                            num_epochs=1),
-            steps=hparams['test_steps'], checkpoint_path="./model/model.ckpt-1576389")
+            steps=hparams['test_steps'], checkpoint_path="./model/model.ckpt-1480862")
         benchmark_logger.log_evaluation_result(eval_result)
 
 
