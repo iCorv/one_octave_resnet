3b9f813ac2ed642fbf47660d544f22eeecac58b8
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-11-28 10:39:58 +0100

    spec + chroma features

--- pop_convnet_main.py
+++ pop_convnet_main.py
@@ -24,10 +24,10 @@ DEFAULT_DTYPE = tf.float32
 
 TEST_ID = 1
 
-train_and_val = True
+train_and_val = False
 predict_flag = False
 train_flag = False
-eval_flag = False
+eval_flag = True
 
 hparams = php.get_hyper_parameters('ConvNet')
 
@@ -79,10 +79,10 @@ def main(_):
     # Evaluate the model.
     if eval_flag:
         eval_result = classifier.evaluate(
-            input_fn=lambda: dataset.tfrecord_val_input_fn(val_dataset_tfrecord,
+            input_fn=lambda: dataset.tfrecord_val_input_fn(test_dataset_tfrecord,
                                                            batch_size=hparams['batch_size'],
                                                            num_epochs=1),
-            steps=hparams['test_steps'], checkpoint_path="./model/model.ckpt-1323466")
+            steps=hparams['test_steps'], checkpoint_path="./model/model.ckpt-1142511")
         benchmark_logger.log_evaluation_result(eval_result)
 
     # 1339892
