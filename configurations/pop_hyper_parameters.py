43d8e2520c9f550dc27a79063fe9437aef1cfd68
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-11-29 16:51:57 +0100

    new resnet architecture

--- configurations/pop_hyper_parameters.py
+++ configurations/pop_hyper_parameters.py
@@ -81,7 +81,7 @@ def get_hyper_parameters(net):
                   'momentum_cycle': [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9],
                   # [momentum for momentum in frange(0.95, 0.85, (0.95-0.85)/(30+2))][0:30] + [momentum for momentum in frange(0.85, 0.95, (0.95-0.85)/(30+2))][0:31],
                   'frames': 5,
-                  'freq_bins': 213,
+                  'freq_bins': 229,
                   'num_channels': 1,
                   'num_classes': 88,
                   'num_examples': num_examples,
