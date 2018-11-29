ddb83df0919233b394f90fae920cf5f10422eeb3
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-11-29 11:42:32 +0100

    renamed

--- pop_model.py
+++ pop_model.py
@@ -339,7 +339,7 @@ def conv_net_kelz(inputs, is_training, data_format='NHWC', batch_size=8, num_cla
 
             # Flatten
             print(net.shape)
-            net = tf.reshape(net, (-1, 64*1*55), 'flatten4')
+            net = tf.reshape(net, (-1, 64*1*51), 'flatten4')
             print(net.shape)
             net = slim.fully_connected(net, 512, scope='fc5')
             print(net.shape)
