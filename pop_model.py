7dc389235f723b4830036532e3414a80065deaa1
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-12-10 10:03:08 +0100

    new input representation

--- pop_model.py
+++ pop_model.py
@@ -471,7 +471,7 @@ def resnet(inputs, is_training, data_format='channels_last', batch_size=8, num_c
                              strides=1, padding='SAME', data_format=data_format)
 
     print(net.shape)
-    net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 2], strides=[2, 2], padding='VALID',
+    net = tf.layers.max_pooling2d(inputs=net, pool_size=[2, 2], strides=[1, 2], padding='VALID',
                                   data_format=data_format)
     print(net.shape)
     net = tf.layers.dropout(net, 0.25, name='dropout2', training=is_training)
@@ -480,7 +480,7 @@ def resnet(inputs, is_training, data_format='channels_last', batch_size=8, num_c
                              data_format=data_format)
 
     print(net.shape)
-    net = tf.layers.max_pooling2d(inputs=net, pool_size=[3, 2], strides=[2, 2], padding='VALID',
+    net = tf.layers.max_pooling2d(inputs=net, pool_size=[2, 2], strides=[1, 2], padding='VALID',
                                   data_format=data_format)
 
     net = tf.layers.dropout(net, 0.25, name='dropout3', training=is_training)
