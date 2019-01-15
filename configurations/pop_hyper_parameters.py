79cdf41d909b3e6213b9e484bfed3dc3da0d1d29
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-01-15 18:59:16 +0100

    resnet training

--- configurations/pop_hyper_parameters.py
+++ configurations/pop_hyper_parameters.py
@@ -10,12 +10,12 @@ DEFAULT_DTYPE = tf.float32
 # fold_3: train - 4366098. valid - 580372, test - 1570005
 # fold_4: train - 4221233. valid - 725237, test - 1570005
 
-num_examples = 4366098
-num_val_examples = 580372
+num_examples = 4249469
+num_val_examples = 697001
 num_test_examples = 1570005
 batch_size = 128
 batches_per_epoch = int(round(num_examples/batch_size))
-train_epochs = 30
+train_epochs = 26
 total_train_steps = train_epochs * batches_per_epoch
 
 
@@ -68,7 +68,7 @@ def get_hyper_parameters(net):
                   # initial learning rate
                   'learning_rate': 1.0,
                   # when to change learning rate
-                  'boundary_epochs': [3, 6, 9, 12, 15, 18, 21, 24, 27],
+                  'boundary_epochs': [3, 6, 9, 12, 15, 18, 20, 22, 24],
                   # [epoch for epoch in frange(0, train_epochs, train_epochs/60)][0:60],
                   # factor by which the initial learning rate is multiplied (needs to be one more than the boundaries)
                   'learning_rate_cycle': [0.1, 0.05, 0.025, 0.0125, 0.00625, 0.003125, 0.0015625, 0.00078125,
