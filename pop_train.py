c964fb76ac4cd1c3e4cdf40d20c16ebad066531e
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-01-15 08:25:22 +0100

    resnet training

--- pop_train.py
+++ pop_train.py
@@ -78,7 +78,7 @@ def main(_):
             input_fn=lambda: dataset.tfrecord_val_input_fn(test_dataset_tfrecord,
                                                            batch_size=hparams['batch_size'],
                                                            num_epochs=1),
-            steps=hparams['test_steps'], checkpoint_path="./model/model.ckpt-1229233")
+            steps=hparams['test_steps'], checkpoint_path="./model/model.ckpt-1089043")
         benchmark_logger.log_evaluation_result(eval_result)
 
     # 1339892
