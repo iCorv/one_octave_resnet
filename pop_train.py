975e41d3a1393d775215220c3fb96d4c0a70ef18
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-01-17 00:48:33 +0100

    resnet training

--- pop_train.py
+++ pop_train.py
@@ -12,8 +12,8 @@ import numpy as np
 import pop_model
 import glob
 
-train_dataset_tfrecord = glob.glob("./tfrecords-dataset/sigtia-configuration2-splits/fold_2/train/*.tfrecords")
-val_dataset_tfrecord = glob.glob("./tfrecords-dataset/sigtia-configuration2-splits/fold_2/valid/*.tfrecords")
+train_dataset_tfrecord = glob.glob("./tfrecords-dataset/sigtia-configuration2-splits/fold_1/train/*.tfrecords")
+val_dataset_tfrecord = glob.glob("./tfrecords-dataset/sigtia-configuration2-splits/fold_1/valid/*.tfrecords")
 test_dataset_tfrecord = glob.glob("./tfrecords-dataset/sigtia-configuration2-splits/fold_1/test/*.tfrecords")
 
 DEFAULT_DTYPE = tf.float32
