2f52ad2c736bc7c165da592f6db379d48bec286c
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-03 16:14:02 +0100

    long input with rnn

--- configurations/pop_hyper_parameters.py
+++ configurations/pop_hyper_parameters.py
@@ -95,7 +95,7 @@ def get_hyper_parameters(net):
                   'eval_steps': int(round(num_val_examples / batch_size)),
                   'test_steps': int(round(num_test_examples / batch_size)),
                   'weight_decay': 1e-7,
-                  'data_format': 'channels_first',  # NHWC (channels last, faster on CPU) or NCHW (channels first, faster on GPU)
+                  'data_format': 'channels_last',  # NHWC (channels last, faster on CPU) or NCHW (channels first, faster on GPU)
                   'train_epochs': train_epochs}
     elif net == 'TCN':
         config = {'batch_size': batch_size,
