c6a8d41b8d7b6583f042c15ffb52984c85985d9d
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-04 13:01:15 +0100

    long input with rnn

--- pop_model.py
+++ pop_model.py
@@ -264,7 +264,7 @@ def conv_net_init(features, labels, mode, learning_rate_fn, loss_filter_fn, weig
         learning_rate = tf.train.exponential_decay(
             0.0006,
             global_step,
-            10000,
+            5000,
             0.98,
             staircase=True)
 
