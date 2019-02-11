4da3a9c2e90e77a261f0986d3ac7e5210555e2a7
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-11 17:22:51 +0100

    note error metrics

--- pop_inference.py
+++ pop_inference.py
@@ -4,6 +4,7 @@ import configurations.pop_hyper_parameters as php
 import numpy as np
 import pop_preprocessing as prep
 import configurations.pop_preprocessing_parameters as ppp
+import pop_utility as util
 from scipy.io import savemat
 
 
@@ -43,10 +44,52 @@ def write_note_activation_to_mat(write_file, base_dir, read_file, audio_config, 
 
     savemat(write_file, {"features": note_activation, "labels": ground_truth})
 
+def get_note_activation(base_dir, read_file, audio_config, norm, context_frames, predictor):
+    """Transforms a wav and mid file to features and writes them to a tfrecords file."""
+    spectrogram = prep.wav_to_spec(base_dir, read_file, audio_config)
+    gt_frame, gt_onset, gt_offset = prep.midi_to_triple_groundtruth(base_dir, read_file, 1. / audio_config['fps'], spectrogram.shape[0])
+    # re-scale spectrogram to the range [0, 1]
+    if norm:
+        spectrogram = np.divide(spectrogram, np.max(spectrogram))
+    note_activation = spectrogram_to_note_activation(spectrogram, context_frames, predictor)
+    return note_activation, gt_frame, gt_onset, gt_offset
+
+
+def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
+    """Error metrics for an entire fold as defined in the preprocessing parameters.
+        fold - Fold.fold_1, Fold.fold_2, Fold.fold_3, Fold.fold_4, Fold.fold_benchmark
+        mode - 'train', 'valid' or 'test' to address the correct config parameter
+        norm - Flag if the spectrogram should be normed to 1
+        net - The network used for classification, e.g. 'ConvNet, 'ResNet_v1'
+        model_dir - e.g. "./model_ResNet_fold_4/". For a specific checkpoint, change the checkpoint number in the
+        chekpoint file from the model folder.
+        save_dir - folder were to save note activations, e.g. "./note_activations/"
+    """
+    config = ppp.get_preprocessing_parameters(fold.value)
+    audio_config = config['audio_config']
+
+    # load fold
+    filenames = open(config[mode + '_fold'], 'r').readlines()
+    filenames = [f.strip() for f in filenames]
+
+    predictor = build_predictor(net, model_dir)
+
+    for file in filenames:
+        # split file path string at "/" and take the last split, since it's the actual filename
+        note_activation, gt_frame, gt_onset, gt_offset = get_note_activation(config['audio_path'], file, audio_config, norm, config['context_frames'], predictor)
+        p_frame, r_frame, f_frame, a_frame = util.eval_framewise(note_activation, gt_frame)
+        p_onset, r_onset, f_onset, a_onset = util.eval_framewise(note_activation, gt_onset)
+        p_offset, r_offset, f_offset, a_offset = util.eval_framewise(note_activation, gt_offset)
+        print("frame F1: " + str(f_frame))
+        print("onset F1: " + str(f_onset))
+        print("offset F1: " + str(f_offset))
+
+
 def get_serving_input_fn(frames, bins):
     def serving_input_fn():
         x = tf.placeholder(dtype=tf.float32, shape=[frames, bins], name='features')
         return tf.estimator.export.TensorServingInputReceiver(x, x)
+
     return serving_input_fn
 
 
@@ -59,7 +102,8 @@ def build_predictor(net, model_dir):
         params=hparams)
 
     estimator_predictor = tf.contrib.predictor.from_estimator(classifier,
-                                                              get_serving_input_fn(hparams['frames'], hparams['freq_bins']),
+                                                              get_serving_input_fn(hparams['frames'],
+                                                                                   hparams['freq_bins']),
                                                               output_key='predictions')
     return estimator_predictor
 
@@ -70,17 +114,11 @@ def get_activation(features, estimator_predictor):
     return p['probabilities']
 
 
-def spectrogram_to_chroma(spec, context_frames, estimator_predictor):
-    chroma = np.zeros([spec.shape[0], 12])
-    for frame in range(context_frames, spec.shape[0] - context_frames):
-        chroma[frame, :] = get_activation(spec[frame - context_frames:frame + context_frames + 1, :], estimator_predictor)
-    log_chroma = np.log(1.0 + chroma)
-    return log_chroma/np.max(log_chroma)
-
-
 def spectrogram_to_note_activation(spec, context_frames, estimator_predictor):
     note_activation = np.zeros([spec.shape[0], 88])
     for frame in range(context_frames, spec.shape[0] - context_frames):
         note_activation[frame, :] = get_activation(spec[frame - context_frames:frame + context_frames + 1, :],
                                                    estimator_predictor)
     return note_activation
+
+
--- pop_utility.py
+++ pop_utility.py
@@ -15,3 +15,46 @@ def find_onset_frame(onset_in_sec, hop_size, sample_rate):
 def midi_to_hz(midi_num, fref=440.0):
     """Transform a midi note to Herz."""
     return np.float_power(2, ((midi_num-69)/12)) * fref
+
+
+def eval_framewise(predictions, targets, thresh=0.5):
+    """
+    """
+    if predictions.shape != targets.shape:
+        raise ValueError('predictions.shape {} != targets.shape {} !'.format(predictions.shape, targets.shape))
+
+    pred = predictions > thresh
+    targ = targets > thresh
+
+    tp = pred & targ
+    fp = pred ^ tp
+    fn = targ ^ tp
+
+    # tp, fp, tn, fn
+    return prf_framewise(tp.sum(), fp.sum(), 0, fn.sum())
+
+
+def prf_framewise(tp, fp, tn, fn):
+    tp, fp, tn, fn = float(tp), float(fp), float(tn), float(fn)
+
+    if tp + fp == 0.:
+        p = 0.
+    else:
+        p = tp / (tp + fp)
+
+    if tp + fn == 0.:
+        r = 0.
+    else:
+        r = tp / (tp + fn)
+
+    if p + r == 0.:
+        f = 0.
+    else:
+        f = 2 * ((p * r) / (p + r))
+
+    if tp + fp + fn == 0.:
+        a = 0.
+    else:
+        a = tp / (tp + fp + fn)
+
+    return p, r, f, a
