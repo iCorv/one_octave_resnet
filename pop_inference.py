5f1c16fbbeeef01a6e3989f24870c19b9275b74e
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-12 14:16:24 +0100

    note error metrics

--- pop_inference.py
+++ pop_inference.py
@@ -74,6 +74,8 @@ def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
     filenames = [f.strip() for f in filenames]
 
     predictor = build_predictor(net, model_dir)
+    proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.5, pre_max=1.0 / audio_config['fps'], post_max=1.0 / audio_config['fps'],
+                                                          delay=0.0, combine=0.03, smooth=0.0, fps=audio_config['fps'])
     frame_wise_metrics = []
     frame_wise_onset_metrics = []
     frame_wise_offset_metrics = []
@@ -92,7 +94,7 @@ def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
         #p_offset, r_offset, f_offset, a_offset = util.eval_frame_wise(np.multiply(note_activation, gt_offset), gt_offset)
         frame_wise_offset_metrics.append(util.eval_frame_wise(np.multiply(note_activation, gt_offset), gt_offset))
 
-
+        print(proc(note_activation))
         #print(p_onset)
         #print(p_offset)
         #print("frame: " + str(frame_wise_metrics[index]))
