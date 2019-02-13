44e5c1eeabfa9623bb71aebd7b8e57cf6293b336
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-13 15:36:37 +0100

    note error metrics

--- pop_inference.py
+++ pop_inference.py
@@ -112,7 +112,8 @@ def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
         offset_predictions = note_activation[1:] - note_activation[0:-1]
         offset_predictions = np.append([np.zeros(offset_predictions[0].shape)], offset_predictions, 0)
         print(np.max(np.max(offset_predictions)))
-        offset_predictions = proc(offset_predictions*-1)
+        #offset_predictions = proc(offset_predictions*-1)
+        offset_predictions = np.greater_equal(offset_predictions*-1, 0.5)
         print(np.shape(offset_predictions))
 
         print(np.sum(np.sum(gt_onset)))
