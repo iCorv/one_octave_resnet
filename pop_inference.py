688db4d8fe9c5434bf2920d83e5945da4f40ca40
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-13 10:56:38 +0100

    note error metrics

--- pop_inference.py
+++ pop_inference.py
@@ -56,14 +56,14 @@ def get_note_activation(base_dir, read_file, audio_config, norm, context_frames,
     # re-scale spectrogram to the range [0, 1]
     if norm:
         spectrogram = np.divide(spectrogram, np.max(spectrogram))
-    #rnn_processor = madmom.features.notes.RNNPianoNoteProcessor()
-    #rnn_act_fn = rnn_processor(os.path.join(base_dir, read_file + '.wav'))
-    #print(rnn_act_fn.shape)
-    #proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.3, fps=100)
-    #onset_predictions = proc(rnn_act_fn)
-    #print(np.shape(onset_predictions))
+    rnn_processor = madmom.features.notes.RNNPianoNoteProcessor()
+    rnn_act_fn = rnn_processor(os.path.join(base_dir, read_file + '.wav'))
+    print(rnn_act_fn.shape)
+    proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.2, fps=100)
+    onset_predictions = proc(rnn_act_fn)
+    print(np.shape(onset_predictions))
     note_activation = spectrogram_to_note_activation(spectrogram, context_frames, predictor)
-    return note_activation, gt_frame, gt_onset, gt_offset
+    return note_activation, gt_frame, gt_onset, gt_offset, onset_predictions
 
 
 def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
@@ -84,7 +84,7 @@ def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
     filenames = [f.strip() for f in filenames]
 
     predictor = build_predictor(net, model_dir)
-    proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.3, fps=audio_config['fps'])
+    #proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.3, fps=100)
     frame_wise_metrics = []
     frame_wise_onset_metrics = []
     frame_wise_offset_metrics = []
@@ -94,7 +94,7 @@ def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
     index = 0
     for file in filenames:
         # split file path string at "/" and take the last split, since it's the actual filename
-        note_activation, gt_frame, gt_onset, gt_offset = get_note_activation(config['audio_path'], file, audio_config,
+        note_activation, gt_frame, gt_onset, gt_offset, onset_predictions = get_note_activation(config['audio_path'], file, audio_config,
                                                                              norm, config['context_frames'], predictor)
         frames = np.greater_equal(note_activation, 0.5)
         # p_frame, r_frame, f_frame, a_frame = util.eval_framewise(note_activation, gt_frame)
@@ -105,7 +105,7 @@ def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
         # p_offset, r_offset, f_offset, a_offset = util.eval_frame_wise(np.multiply(note_activation, gt_offset), gt_offset)
         frame_wise_offset_metrics.append(util.eval_frame_wise(np.multiply(note_activation, gt_offset), gt_offset))
 
-        onset_predictions = proc(note_activation)
+        #onset_predictions = proc(note_activation)
         print(np.sum(np.sum(gt_onset)))
         ref_intervals, ref_pitches = util.pianoroll_to_interval_sequence(gt_frame,
                                                                          frames_per_second=audio_config['fps'],
