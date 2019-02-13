dbeb01183aa344d2d5ac8c04867af331ae30f6d2
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-13 09:25:30 +0100

    note error metrics

--- pop_inference.py
+++ pop_inference.py
@@ -77,7 +77,7 @@ def compute_all_error_metrics(fold, mode, net, model_dir, save_dir, norm=False):
     filenames = [f.strip() for f in filenames]
 
     predictor = build_predictor(net, model_dir)
-    proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.7, pre_max=1.0 / audio_config['fps'],
+    proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.5, pre_max=1.0 / audio_config['fps'],
                                                           post_max=1.0 / audio_config['fps'],
                                                           delay=0.0, combine=0.03, smooth=0.0, fps=audio_config['fps'])
     frame_wise_metrics = []
