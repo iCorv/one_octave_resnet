179dbee8db53b6990274c76bea2021ec8267d9ef
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-13 14:25:44 +0100

    note error metrics

--- pop_inference.py
+++ pop_inference.py
@@ -64,7 +64,10 @@ def get_note_activation(base_dir, read_file, audio_config, norm, context_frames,
     max_act_fn = np.max(np.max(rnn_act_fn))
     print(max_act_fn)
     #proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.1, fps=100)
-    offset_predictions = proc((rnn_act_fn * (-1)) + max_act_fn)
+    
+    rnn_act_fn = (rnn_act_fn * (-1)) + max_act_fn
+    print(np.max(np.max(rnn_act_fn)))
+    offset_predictions = proc(rnn_act_fn)
     print(np.shape(onset_predictions))
     print(np.shape(offset_predictions))
     note_activation = spectrogram_to_note_activation(spectrogram, context_frames, predictor)
