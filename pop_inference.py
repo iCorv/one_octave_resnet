195cdf115714ae89f70ebf075c30943150103834
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-15 11:05:20 +0100

    note error metrics

--- pop_inference.py
+++ pop_inference.py
@@ -331,7 +331,7 @@ def transcribe_piano_piece(audio_file, net, model_dir, save_dir, onset_duration_
     # init piano note processor for onset prediction
     rnn_processor = madmom.features.notes.RNNPianoNoteProcessor()
 
-    spectrogram = prep.wav_to_spec("", audio_file, audio_config)
+    spectrogram = prep.wav_to_spec("", audio_file.split('.')[0], audio_config)
 
     # re-scale spectrogram to the range [0, 1]
     if norm:
@@ -381,7 +381,7 @@ def transcribe_piano_piece(audio_file, net, model_dir, save_dir, onset_duration_
     notes = np.stack((est_intervals[:, 0], est_pitches, est_intervals[:, 1]-est_intervals[:, 0]), axis=1)
     notes_onset_pred = np.stack((est_intervals_onset_pred[:, 0], est_pitches_onset_pred, est_intervals_onset_pred[:, 1] - est_intervals_onset_pred[:, 0]), axis=1)
     notes_onset_pred_heuristic = np.stack((est_intervals_onset_pred_heuristic[:, 0], est_pitches_onset_pred_heuristic, est_intervals_onset_pred_heuristic[:, 1] - est_intervals_onset_pred_heuristic[:, 0]), axis=1)
-    madmom.io.midi.write_midi(notes, save_dir + audio_file.split('/')[-1] + "_noOnset", duration=0.6, velocity=100)
+    madmom.io.midi.write_midi(notes, save_dir + (audio_file.split('/')[-1]).split('.')[0] + "_noOnset", duration=0.6, velocity=100)
     madmom.io.midi.write_midi(notes_onset_pred, save_dir + audio_file.split('/')[-1] + "_onsetPrediction", duration=0.6, velocity=100)
     madmom.io.midi.write_midi(notes_onset_pred_heuristic, save_dir + audio_file.split('/')[-1] + "_onsetHeuristic", duration=0.6, velocity=100)
 
