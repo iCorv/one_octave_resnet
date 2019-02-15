91f2fef47c583d93646c6ca664623cdef78b08fb
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-02-15 13:01:24 +0100

    note error metrics

--- pop_inference.py
+++ pop_inference.py
@@ -327,7 +327,7 @@ def transcribe_piano_piece(audio_file, net, model_dir, save_dir, onset_duration_
     # build predictor
     predictor = build_predictor(net, model_dir)
     # init madmom peak picker
-    proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.1, fps=100)
+    proc = madmom.features.notes.NotePeakPickingProcessor(threshold=0.01, fps=100)
     # init piano note processor for onset prediction
     rnn_processor = madmom.features.notes.RNNPianoNoteProcessor()
 
