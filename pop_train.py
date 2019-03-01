16ec9af486d27f9be059f0466dc6570493b068b2
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2019-03-01 21:32:57 +0100

    new preprocessing

--- pop_train.py
+++ pop_train.py
@@ -26,7 +26,7 @@ predict_flag = False
 train_flag = False
 eval_flag = False
 
-hparams = php.get_hyper_parameters('ResNet_v1')
+hparams = php.get_hyper_parameters('ResNet_v1_RNN')
 
 
 def main(_):
